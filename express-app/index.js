4c0ea2ff16fc622e7be9d3edbb48f37aa43b6c2d
alamincse97 <alamin.cse97@gmail.com>
2026-08-18 05:00:16 +0000

    [ADD] Express app index file create

---
+++ express-app/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the Express Backend!');
+});
+
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`);
+});
